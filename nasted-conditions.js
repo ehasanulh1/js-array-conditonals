169f96ad8f771eae0d7232da247d7a5d28fcb316
ehasanulh1 <ehasanulh1@gmail.com>
2022-08-01 18:39:48 +0600

    today have finised

---
+++ nasted-conditions.js
@@ -0,0 +1,20 @@
+var math = true;
+var geometry = true;
+var straightLint = false;
+
+if (math == true) {
+    if (geometry == true) {
+        if (straightLint == false) {
+            console.log('shoja pothe cholo')
+        }
+        else {
+            console.log('baka pothe cholbana')
+        }
+    }
+    else {
+        console.log('pithagorous hoite parbana')
+    }
+}
+else {
+    console.log('sob ses')
+}
