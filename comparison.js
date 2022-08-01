757765a4ac5a7020d31f8ff0cb5b03b9d3fd8054
ehasanulh1 <ehasanulh1@gmail.com>
2022-08-01 19:40:04 +0600

    today this finish

--- comparison.js
+++ comparison.js
@@ -1,23 +1,23 @@
-//console.log(5 < 6);
-//console.log(5 > 6);
-//console.log(5 == 6);
-//console.log(6 == 6);
-//console.log(6 != 6);
-//console.log(5 != 6);
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(6 != 6);
+// console.log(5 != 6);
 
-//console.log(5 <= 6);
-//console.log(6 >= 6);
+// console.log(5 <= 6);
+ console.log(6 >= 6);
 
-var num1 = 56;
-var num2 = 46;
-console.log(num1 > num2);
-console.log(num1 < num2);
-console.log(num1 >= num2);
-console.log(num1 != num2);
-console.log(num1 == num2);
+// var num1 = 56;
+// var num2 = 46;
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 >= num2);
+// console.log(num1 != num2);
+// console.log(num1 == num2);
 
 // multiple conditions
 // must fulfil those conditions
-money1 > money2 && result1 > result2 && height1 > height2
+// money1 > money2 && result1 > result2 && height1 > height2
 // can fulfil one of those conditions
-money1 > money2 || result1 > result2 || height1 > height2
+// money1 > money2 || result1 > result2 || height1 > height2
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var numbers = [45, 68, 78, 56, 89, 98];
 
 // 1. get element by index
-var element = numbers[1];
+// var element = numbers[1];
 // console.log(element);
 
 // 2. (set/change) element value by index
-numbers[1] = 77;
-numbers[3] = 50;
+// numbers[1] = 77;
+// numbers[3] = 50;
 // console.log(numbers);
 
 // 3. find index of an element
